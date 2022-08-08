9d196733329e1bd24f15de11248203711d58c25f
April G Tibbs <mrztibbs@gmail.com>
2022-08-08 18:55:13 -0500

    Lesson 3 exercise

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,55 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = false;
+
+
+  moveRocket(rocketImage, direction: string){
+    if (direction ==='right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left=movement;
+    } else if (direction ==='left'){
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left=movement;
+    } else if (direction ==='up'){
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom=movement;
+    } else if (direction ==='down'){
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom=movement;
+    } 
+}
+
+handleTakeOff() {
+  let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+  if (result) {
+     this.color = 'blue';
+     this.height = 10000;
+     this.width = 0;
+     this.message = 'Shuttle in flight.';
+  }
+}
+
+abortMission(rocketImage) {
+  let result = window.confirm('Abort Mission?');
+  if (result) {
+     this.color = 'green';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Mission Aborted!';
+     rocketImage.style.bottom = '0px';
+  }
+}
+
+landRocket(rocketImage) {
+  let result = window.confirm('Abort Mission?');
+  if (result) {
+     this.color = 'green';
+     this.height = 0;
+     this.width = 0;
+     this.message = 'Landing!';
+     rocketImage.style.left=0;
+     rocketImage.style.bottom = '0px';
+  }
+}
 }
